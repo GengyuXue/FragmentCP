a8a2dee70aa23990bf2e54a953072599a1e75f74
Haotian Xu <haotian.xu2@icloud.com>
2024-04-03 10:37:35 +0200

    update

--- R/simulate.R
+++ R/simulate.R
@@ -1,6 +1,6 @@
 # function to generate functional data observed on fragments
 #' @export
-fragment_data <- function(mu = 0, cov = periodic_cov_fct, r = 5, sigma, n = 100, m = 5, sigma_epsilon = 1, domain = c(0, 1), delta = 0.3){
+fragment_data1 <- function(mu = 0, r = 5, sigma, n = 100, m = 5, sigma_epsilon = 1, domain = c(0, 1), delta = 0.3){
   Ly <- matrix(0, n, m)
   L <- domain[2] - domain[1]
   a_vec <- runif(n, min = domain[1], max = domain[2] - delta * L)
@@ -9,12 +9,17 @@ fragment_data <- function(mu = 0, cov = periodic_cov_fct, r = 5, sigma, n = 100,
     a + sort(runif(m, min = 0, max = delta * L))
   }))
   Ly0 <- t(apply(Lt, 1, function(tobs) {
+    m = length(tobs)
     if(is.function(mu)) 
       mui <- mu(tobs)
     else if(length(mu) == 1) 
       mui <- rep(mu, length(tobs))
     else stop("mu must be a scalar or a function.")
-    y0 <- mui + gaussian_process(cov, 1, tobs, r, sigma)
+    dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
+    C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r)))
+    temp = evaluate_basis(r, c(0,1), tobs)
+    Sigma_mat = temp %*% C_mat %*% t(temp)
+    y0 <- mui + MASS::mvrnorm(n = 1, mu = rep(0, m), Sigma = Sigma_mat*sigma^2)
     return(c(y0))
   }))
   Ly <- Ly0 + matrix(rnorm(n = n*m, sd = sigma_epsilon), n, m)
@@ -27,26 +32,58 @@ fragment_data <- function(mu = 0, cov = periodic_cov_fct, r = 5, sigma, n = 100,
 }
 
 
+#' @export
+fragment_data2 <- function(mu = 0, r = 5, sigma, n = 100, m = 5, sigma_epsilon = 1, domain = c(0, 1), delta = 0.3){
+  Ly <- matrix(0, n, m)
+  L <- domain[2] - domain[1]
+  a_vec <- runif(n, min = domain[1], max = domain[2] - delta * L)
+  Lt <- t(sapply(1:n, function(i) {
+    a <- a_vec[i]
+    a + sort(runif(m, min = 0, max = delta * L))
+  }))
+  Ly0 <- t(apply(Lt, 1, function(tobs) {
+    m = length(tobs)
+    if(is.function(mu)) 
+      mui <- mu(tobs)
+    else if(length(mu) == 1) 
+      mui <- rep(mu, length(tobs))
+    else stop("mu must be a scalar or a function.")
+    dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
+    C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(r:1)))
+    temp = evaluate_basis(r, c(0,1), tobs)
+    Sigma_mat = temp %*% C_mat %*% t(temp)
+    y0 <- mui + MASS::mvrnorm(n = 1, mu = rep(0, m), Sigma = Sigma_mat*sigma^2)
+    return(c(y0))
+  }))
+  Ly <- Ly0 + matrix(rnorm(n = n*m, sd = sigma_epsilon), n, m)
+  Lr <- NULL
+  for(i in 1:n){
+    Lr = cbind(Lr, tcrossprod(Ly[i,]))
+  }
+  R <- list(t = Lt, y = Ly, r = Lr)
+  return(R)
+}
+
 #dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
 #C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r)))
 
-#' @export
-periodic_cov_fct1 <- function(tObs, r = 5){
-  dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
-  C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r)))
-  temp = evaluate_basis(r, c(0,1), tObs)
-  C = temp %*% C_mat %*% t(temp)
-  return(C)
-}
-
-#' @export
-periodic_cov_fct2 <- function(tObs, r = 5){
-  dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
-  C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r)))
-  temp = evaluate_basis(r, c(0,1), tObs)
-  C = temp %*% C_mat %*% t(temp)
-  return(C)
-}
+#' #' @export
+#' periodic_cov_fct1 <- function(tObs, r = 5){
+#'   dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
+#'   C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r)))
+#'   temp = evaluate_basis(r, c(0,1), tObs)
+#'   C = temp %*% C_mat %*% t(temp)
+#'   return(C)
+#' }
+#' 
+#' #' @export
+#' periodic_cov_fct2 <- function(tObs, r = 5){
+#'   dist_mat = dist(1:r, method = "manhattan", diag = T, upper = T)
+#'   C_mat = as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(r:1)))
+#'   temp = evaluate_basis(r, c(0,1), tObs)
+#'   C = temp %*% C_mat %*% t(temp)
+#'   return(C)
+#' }
 
 
 shrink <- function(Lt,ext){
@@ -54,7 +91,7 @@ shrink <- function(Lt,ext){
 }
 
 #' @export
-predict_cov <- function(grids, C_mat, ext){
+predict_cov <- function(grids, C_mat, ext = 0){
   r <- dim(C_mat)[1]
   if(ext > 0) grids <- shrink(grids, ext)
   B <- evaluate_basis(r, c(0,1), grid = grids)
@@ -67,13 +104,13 @@ predict_cov <- function(grids, C_mat, ext){
 # }
 
 
-#' @export
-gaussian_process <- function(cov_fct, n, tObs, r, sigma){
-  m = length(tObs)
-  Sigma <- cov_fct(tObs, r)
-  result = MASS::mvrnorm(n = n, mu = rep(0, m), Sigma = Sigma*sigma^2)
-  return(result)
-}
+#' #' @export
+#' gaussian_process <- function(cov_fct, n, tObs, r, sigma){
+#'   m = length(tObs)
+#'   Sigma <- cov_fct(tObs, r)
+#'   result = MASS::mvrnorm(n = n, mu = rep(0, m), Sigma = Sigma*sigma^2)
+#'   return(result)
+#' }
 
 #' @export
 part2local = function(parti_vec){
--- other/numeric.R
+++ other/numeric.R
@@ -1,182 +1,201 @@
-lambda = 0.00001
-ext = 0.1
+lambda = 0.0000001
+ext = 0.05
 maxIt = 1
-r1 = 5
-r2 = 5
-sigma1 = 0.3
-sigma2 = 0.7
-Delta = 20
+r1 = 2
+r2 = 2
+sigma1 = 1
+sigma2 = 1
+Delta = 50
 
-
-data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 100, m = 20, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
-data2 = fragment_data(mu = 0, cov = periodic_cov_fct2, r = r2, sigma2, n = 100, m = 20, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
-
+# simulate data with one change point
+data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 100, m = 10, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
+data2 = fragment_data2(mu = 0, r = r2, sigma2, n = 100, m = 10, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
 data = list("t"= rbind(data1$t, data2$t), "y" = rbind(data1$y, data2$y), "r" = cbind(data1$r, data2$r))
 
-lambda = 0.00001
-cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
+
+regular_grid = seq(0.05, 0.95, length.out = 50)
+# obtain the estimated covariance function
+cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt) # estimate covariance function
 C_est1 <- cov.obj1$C
+cov_est1 = predict_cov(regular_grid, C_est1)
+# obtain the true covariance function
 dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
 C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
-cov1 = periodic_cov_fct1(seq(0.05, 0.95, length.out = 50), r = r1)*sigma1^2
-sum((cov_est1 - cov1)^2)
-sum((cov_est1 - cov1)^2)/sum(cov1^2)
+cov1 = predict_cov(regular_grid, C_1)
+# compare
+mean((cov_est1 - cov1)^2)
+
 
 cov.obj2 = cov_basis(data2$t, data2$y, data2$r, r2, lambda, ext, maxIt)
 C_est2 <- cov.obj2$C
+cov_est2 = predict_cov(regular_grid, C_est2)
 dist_mat = dist(1:r2, method = "manhattan", diag = T, upper = T)
-C_2 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r2)))
-cov_est2 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est2, ext)
-cov2 = periodic_cov_fct2(seq(0.05, 0.95, length.out = 50), r = r2)*sigma2^2
-sum((cov_est2 - cov2)^2)
-sum((cov_est2 - cov2)^2)/sum(cov2^2)
+C_2 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(r2:1)))
+cov2 = predict_cov(regular_grid, C_2)
+mean((cov_est2 - cov2)^2)
+#sum((cov_est2 - cov2)^2)/sum(cov2^2)
+
+mean((cov1 - cov2)^2)
+mean((cov_est1 - cov_est2)^2)
+
 
 cov.obj = cov_basis(data$t, data$y, data$r, r1, lambda, ext, maxIt)
 C_est <- cov.obj$C
-cov_est = predict_cov(seq(0.05, 0.95, length.out = 50), C_est, ext)
-sum((cov_est - cov1)^2)
-sum((cov_est - cov2)^2)
-sum((cov_est - (cov1+cov2)/2)^2)
+cov_est = predict_cov(regular_grid, C_est, ext)
+mean((cov_est - cov1)^2)
+mean((cov_est - cov2)^2)
+mean((cov_est - (cov1+cov2)/2)^2)
 
-cpt_result = DP_fragment(data$t, data$y, data$r, r = 5, lambda, xi = 0.2, ext, maxIt, Delta)
+cpt_result = DP_fragment(data$t, data$y, data$r, r = 2, lambda, xi = 0.5, ext, maxIt, Delta)
 part2local(cpt_result$partition)
 
 
 B = 200
 lambda = 0.00001
-cov1 = periodic_cov_fct1(seq(0.05, 0.95, length.out = 50), r = r1)*sigma1^2
+regular_grid = seq(0.05, 0.95, length.out = 50)
 results_n100_m5 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 100, m = 5, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 100, m = 5, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n100_m5[b,1] = cov.obj1$error
-  results_n100_m5[b,2] = sum((cov_est1 - cov1)^2)
+  results_n100_m5[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n100_m10 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 100, m = 10, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 100, m = 10, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n100_m10[b,1] = cov.obj1$error
-  results_n100_m10[b,2] = sum((cov_est1 - cov1)^2)
+  results_n100_m10[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n100_m15 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 100, m = 15, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 100, m = 15, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n100_m15[b,1] = cov.obj1$error
-  results_n100_m15[b,2] = sum((cov_est1 - cov1)^2)
+  results_n100_m15[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n100_m20 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 100, m = 20, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 100, m = 20, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n100_m20[b,1] = cov.obj1$error
-  results_n100_m20[b,2] = sum((cov_est1 - cov1)^2)
+  results_n100_m20[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n150_m5 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 150, m = 5, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 150, m = 5, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n150_m5[b,1] = cov.obj1$error
-  results_n150_m5[b,2] = sum((cov_est1 - cov1)^2)
+  results_n150_m5[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n150_m10 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 150, m = 10, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 150, m = 10, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n150_m10[b,1] = cov.obj1$error
-  results_n150_m10[b,2] = sum((cov_est1 - cov1)^2)
+  results_n150_m10[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n150_m15 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 150, m = 15, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 150, m = 15, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n150_m15[b,1] = cov.obj1$error
-  results_n150_m15[b,2] = sum((cov_est1 - cov1)^2)
+  results_n150_m15[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n150_m20 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 150, m = 20, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 150, m = 20, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n150_m20[b,1] = cov.obj1$error
-  results_n150_m20[b,2] = sum((cov_est1 - cov1)^2)
+  results_n150_m20[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n200_m5 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 200, m = 5, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 200, m = 5, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n200_m5[b,1] = cov.obj1$error
-  results_n200_m5[b,2] = sum((cov_est1 - cov1)^2)
+  results_n200_m5[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n200_m10 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 200, m = 10, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 200, m = 10, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n200_m10[b,1] = cov.obj1$error
-  results_n200_m10[b,2] = sum((cov_est1 - cov1)^2)
+  results_n200_m10[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n200_m15 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 200, m = 15, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 200, m = 15, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n200_m15[b,1] = cov.obj1$error
-  results_n200_m15[b,2] = sum((cov_est1 - cov1)^2)
+  results_n200_m15[b,2] = mean((cov_est1 - cov1)^2)
 }
 results_n200_m20 = matrix(NA, B, 2)
 for(b in 1:B){
-  data1 = fragment_data(mu = 0, cov = periodic_cov_fct1, r = r1, sigma1, n = 200, m = 20, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
+  data1 = fragment_data1(mu = 0, r = r1, sigma1, n = 200, m = 20, sigma_epsilon = 0.1, domain = c(0, 1), delta = 0.3)
   cov.obj1 = cov_basis(data1$t, data1$y, data1$r, r1, lambda, ext, maxIt)
   C_est1 <- cov.obj1$C
+  cov_est1 = predict_cov(regular_grid, C_est1)
   dist_mat = dist(1:r1, method = "manhattan", diag = T, upper = T)
   C_1 <- as.matrix(2^(-dist_mat - 5/2)) + diag(1.5^(1-(1:r1)))
-  cov_est1 = predict_cov(seq(0.05, 0.95, length.out = 50), C_est1, ext)
+  cov1 = predict_cov(regular_grid, C_1)
   results_n200_m20[b,1] = cov.obj1$error
-  results_n200_m20[b,2] = sum((cov_est1 - cov1)^2)
+  results_n200_m20[b,2] = mean((cov_est1 - cov1)^2)
 }
 boxplot(results_n100_m5[,1]/(100), results_n100_m10[,1]/(100), results_n100_m15[,1]/(100), results_n100_m20[,1]/(100), results_n150_m5[,1]/(150), results_n150_m10[,1]/(150), results_n150_m15[,1]/(150), results_n150_m20[,1]/(150), results_n200_m5[,1]/(200), results_n200_m10[,1]/(200), results_n200_m15[,1]/(200), results_n200_m20[,1]/(200))
 boxplot(results_n100_m5[,2]/50, results_n100_m10[,2]/50, results_n100_m15[,2]/50, results_n100_m20[,2]/50, results_n150_m5[,2]/50, results_n150_m10[,2]/50, results_n150_m15[,2]/50, results_n150_m20[,2]/50, results_n200_m5[,2]/50, results_n200_m10[,2]/50, results_n200_m15[,2]/50, results_n200_m20[,2]/50)
@@ -190,7 +209,6 @@ boxplot(results_n100_m5[,2]/50, results_n100_m10[,2]/50, results_n100_m15[,2]/50
 
 B = 200
 lambda = 0.00001
-cov2 = periodic_cov_fct2(seq(0.05, 0.95, length.out = 50), r = r2)*sigma2^2
 results2_n100_m5 = matrix(NA, B, 2)
 for(b in 1:B){
   data2 = fragment_data(mu = 0, cov = periodic_cov_fct2, r = r2, sigma2, n = 100, m = 5, sigma_epsilon = 0.5, domain = c(0, 1), delta = 0.3)
